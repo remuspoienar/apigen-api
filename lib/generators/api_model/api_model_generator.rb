5644ef4f9e6cf00106afedc3d78f2585db26b89b
Remus Poienar <remuspoienar@yahoo.com>
2017-05-23 00:34:19 +0300

    Skip creation of new files in apigen project

--- lib/generators/api_model/api_model_generator.rb
+++ lib/generators/api_model/api_model_generator.rb
@@ -1,9 +1,12 @@
 class ApiModelGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::ResourceHelpers
+
   source_root File.expand_path('../templates', __FILE__)
 
   def create_api_model_file
     file_content = File.open(File.expand_path('../templates/api_model.rb', __FILE__)).read
-    create_file "api_projects/#{api_project.formatted_name}/app/models/#{file_name}.rb", eval(file_content)
+    create_file "app/models/#{file_name}.rb", eval(file_content)
+    # create_file "api_projects/#{api_project.formatted_name}/app/models/#{file_name}.rb", eval(file_content)
   end
 
   private
@@ -11,9 +14,9 @@ class ApiModelGenerator < Rails::Generators::NamedBase
   def args_hash
     @args_hash ||= Hash[args.map{|arg| [arg.split('=')[0], arg.split('=')[1]]}]
   end
-  
+
   def api_project
     @api_project ||= ApiProject.find(args_hash['api_project'])
   end
-  
+
 end
